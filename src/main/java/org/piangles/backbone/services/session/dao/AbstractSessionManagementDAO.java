7b476d11acd5f2cb849cc178523ab9300be97eda
Budhaditya Biswas <budhaditya.biswas@avalara.com>
2024-11-21 14:18:24 +0530

    bypass for existing sessions

--- src/main/java/org/piangles/backbone/services/session/dao/AbstractSessionManagementDAO.java
+++ src/main/java/org/piangles/backbone/services/session/dao/AbstractSessionManagementDAO.java
@@ -21,6 +21,7 @@ package org.piangles.backbone.services.session.dao;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.piangles.backbone.services.session.SessionDetails;
 import org.piangles.core.dao.DAOException;
 
@@ -79,6 +80,10 @@ public abstract class AbstractSessionManagementDAO implements SessionManagementD
 		if (sessionDetails != null && sessionDetails.getUserId().equals(userId) && sessionDetails.getBizId().equals(bizId))
 		{
 			if (!isSessionValid(sessionDetails.getLastAccessedTS()))
+			{
+				sessionDetails = null;
+			}
+			else if (!StringUtils.isEmpty(sessionDetails.getBizId()) && !sessionDetails.getBizId().equals(bizId))
 			{
 				sessionDetails = null;
 			}
